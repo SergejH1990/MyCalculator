5e55a9210c0720a68faa333117c520b5a8cf183c
SergejH1990 <seho1990@gmx.de>
2025-10-05 12:30:24 +0200

    code style

--- main.cpp
+++ main.cpp
@@ -8,11 +8,13 @@ int main(int argc, char *argv[])
 {
 	QApplication app(argc, argv);
 
-	// Load style sheat from file
-	QFile File(":/stylesheet.qss");
-	File.open(QFile::ReadOnly);
-	QString StyleSheet = QLatin1String(File.readAll());
-	app.setStyleSheet(StyleSheet);
+	// Load style sheet from file
+	{
+		QFile File(":/stylesheet.qss");
+		File.open(QFile::ReadOnly);
+		const QString StyleSheet = QLatin1String(File.readAll());
+		app.setStyleSheet(StyleSheet);
+	}
 
 	TaschenrechnerW MyCalculator;
 	MyCalculator.show();
