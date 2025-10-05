2a935dff9a9a648a9d1256ccb118110b42f8026a
SergejH1990 <seho1990@gmx.de>
2025-10-05 12:43:19 +0200

    code style

--- taschenrechnerw.h
+++ taschenrechnerw.h
@@ -20,41 +20,45 @@ protected:
 	enum class MathOperationList
 	{
 		None,
-        Plus,
-        Minus,
-        Multiply,
-        Divide
-    };
-
-    class QWidget* calculatorWidget;
-    class QLabel* calculatorDisplay;
-    class QVBoxLayout* verticalLayout;
-    class QHBoxLayout* buttonRow0;
-    class QHBoxLayout* buttonRow1;
-    class QHBoxLayout* buttonRow2;
-    class QHBoxLayout* buttonRow3;
-    class QHBoxLayout* buttonRow4;
-    std::array<class QPushButton*, 10> numberButtons;
-    class QPushButton* plusButton;
-    class QPushButton* minusButton;
-    class QPushButton* multiplyButton;
-    class QPushButton* divideButton;
-    class QPushButton* signChangeButton;
-    class QPushButton* equalButton;
-    class QPushButton* deleteButton;
-    class QPushButton* commaButton;
+		Plus,
+		Minus,
+		Multiply,
+		Divide
+	};
 
 	void numberButtonPressed(const int number);
-    void singleOutputManipulation();
-    void mathematicalOperation();
-    void evaluateResult();
-    void resetButtons();
-    MathOperationList GetEnumValueFromString(const QString& buttonName);
+	void singleOutputManipulation();
+	void mathematicalOperation();
+	void evaluateResult();
+	void resetButtons();
+	MathOperationList GetEnumValueFromString(const QString& buttonName);
 
-    double screenNumber;
+	class QWidget* calculatorWidget;
+	class QLabel* calculatorDisplay;
+	QMessageBox Box;
+	class QVBoxLayout* verticalLayout;
+
+	// Button layouts
+	class QHBoxLayout* buttonRow0;
+	class QHBoxLayout* buttonRow1;
+	class QHBoxLayout* buttonRow2;
+	class QHBoxLayout* buttonRow3;
+	class QHBoxLayout* buttonRow4;
+
+	// Buttons
+	std::array<class QPushButton*, 10> numberButtons;
+	class QPushButton* plusButton;
+	class QPushButton* minusButton;
+	class QPushButton* multiplyButton;
+	class QPushButton* divideButton;
+	class QPushButton* signChangeButton;
+	class QPushButton* equalButton;
+	class QPushButton* deleteButton;
+	class QPushButton* commaButton;
+
+	double screenNumber;
 	std::optional<double> firstOperatorNumber;
 	bool isSecondInputNumber;
-    MathOperationList trackOperationButton;
-    QMessageBox Box;
+	MathOperationList trackOperationButton;
 };
 #endif // TASCHENRECHNERW_H
